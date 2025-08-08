0bb0c4817c54dea54c756bb8d390e4679053de20
sairam <mr.sairamofficial@gmail.com>
2025-08-08 12:41:50 +0530

    database configuration

--- Backend/server.js
+++ Backend/server.js
@@ -1,41 +1,57 @@
-import express from 'express'; 
+import express from 'express';
 import "dotenv/config";
 import cors from "cors";
-import fetch from 'node-fetch';
+import mongoose, { connect } from 'mongoose';
+import chatRoutes from "./routes/chat.js"
+import Thread from './models/Thread.js';
+
+
+
 
 const port = 8080;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use("/api",chatRoutes);
 
 app.listen(port, () => {
     console.log(`App is Listening at port : ${port}`);
+    connectDB();
 });
 
-app.post("/test", async (req, res) => {
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.OPENAI_API}`,
-        },
-        body: JSON.stringify({
-            model: "gpt-4o-mini", 
-            messages: [{
-                role: "user",
-                content: req.body.message,
-            }]
-        })
-    };
-
+const connectDB = async () => {
     try {
-        const response = await fetch("https://api.openai.com/v1/chat/completions", options);
-        const data = await response.json();
-        //console.log(data.choices[0].message.content); //reply
-        res.send(data.choices[0].message.content);
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("Connected to DataBase SuccessFully")
     } catch (err) {
-        console.error("Error:", err); 
-        res.status(500).json({ error: "Something went wrong." });
+        console.log("Failed to Connect with database", err);
     }
-});
+}
+
+// app.post("/test", async (req, res) => {
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Authorization": `Bearer ${process.env.OPENAI_API}`,
+//         },
+//         body: JSON.stringify({
+//             model: "gpt-4o-mini",
+//             messages: [{
+//                 role: "user",
+//                 content: req.body.message,
+//             }]
+//         })
+//     };
+
+//     try {
+//         const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+//         const data = await response.json();
+//         //console.log(data.choices[0].message.content); //reply
+//         res.send(data.choices[0].message.content);
+//     } catch (err) {
+//         console.error("Error:", err);
+//         res.status(500).json({ error: "Something went wrong." });
+//     }
+// });
